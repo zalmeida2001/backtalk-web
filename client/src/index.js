9d4871907a7f9b1d8c769439e04add23a5554d19
Krokzter <krokfear@gmail.com>
2021-06-20 21:40:32 +0100

    Fixed 'favicon' crash

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom'
 import Routes from './Routes'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-<link rel="shortcut icon" href="public/favicon.ico"></link>
-
 ReactDOM.render(<Routes />, document.getElementById('root'))
