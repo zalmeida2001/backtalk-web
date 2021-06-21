37ed1558fd675b0282716762492a753269274709
Krokzter <krokfear@gmail.com>
2021-06-21 22:51:20 +0100

    UI prototype

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -6,13 +6,11 @@ import AuthRoute from './components/AuthRoute'
 import Login from './pages/Login'
 import Messages from './pages/Messages'
 import Register from './pages/Register'
-import Chat from "./pages/Chat"
 
 const Routes = () => {
 
   return <BrowserRouter>
     <Switch>
-      <ProtectedRoute exact path="/messages/:roomId" component={Chat} />
       <ProtectedRoute path="/messages" component={Messages} />
       <AuthRoute exact path="/login" component={Login} />
       <AuthRoute exact path="/register" component={Register} />
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
-import { Link, withRouter } from 'react-router-dom'
-import { Container, Button, Tab, Nav, Modal } from 'react-bootstrap'
+import { withRouter } from 'react-router-dom'
+import UseChat from "../components/UseChat"
 import '../Styles.css'
-import Chat from "./Chat"
 
-const Messages = () => {
+const Messages = (props) => {
 
-  const [User, setUser] = useState("Messages | BackTalk")
-  document.title = User
+  const { roomId } = props.match.params
+  const { messages, sendMessage } = UseChat(roomId)
+  const [newMessage, setNewMessage] = useState('')
+  const [User, setUser] = useState('')
+  document.title = "Messages | BackTalk"
 
   Axios.defaults.withCredentials = true
   useEffect(() => {
     Axios.get("http://localhost:3001/auth").then((response) => {
       if (response.data.loggedIn === true)
-        setUser("BackTalk - " + response.data.user[0].username)
+        setUser(response.data.user[0].username)
     })
   })
 
@@ -23,31 +25,76 @@ const Messages = () => {
     window.location = '/login'
   }
 
-  const [roomName, setRoomName] = useState("");
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value)
+  }
 
-  const handleRoomNameChange = (event) => {
-    setRoomName(event.target.value)
+  const handleSendMessage = () => {
+    sendMessage(User + ": " + newMessage)
+    setNewMessage("")
   }
 
   return (
-    <Chat />
-    /*
-    <div className="home-container">
-      <input
-        type="text"
-        placeholder="Room"
-        value={roomName}
-        onChange={handleRoomNameChange}
-        className="text-input-field"
-      />
-      <Link to={`/messages/${roomName}`} className="enter-room-button">
-        Join room
-      </Link>
-      <Container>
-        <Button className="mr-3" variant="danger" onClick={logout}>Logout</Button>
-      </Container>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2" style={{ height: '100vh' }}>
+          <div className="card bg-default h-100 d-flex flex-column">
+            <h5 className="card-header">
+              BackTalk
+            </h5>
+            <div className="overflow-auto flex-grow-1">
+              <div className="card-body border-bottom">
+                <p className="card-text">
+                  Card content
+                </p>
+              </div>
+            </div>
+            <div className="card-footer">
+              <div className="btn-group btn-group-lg" role="group">
+                <button className="btn btn-primary rounded" type="button">
+                  Adicionar
+                </button>
+                <button className="btn btn-danger rounded" type="button" onClick={logout}>
+                  Logout
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-10">
+          <div className="h-100 d-flex flex-column">
+            <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3">
+              {messages.map((message, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={`my-1 d-flex flex-column ${message.ownedByCurrentUser ? 'align-self-end align-items-end' : 'align-items-start'}`}
+                  >
+                    <div key={i} className={`rounded px-2 py-1 message-item ${message.ownedByCurrentUser ? "bg-primary text-white" : "bg-secondary text-black"}`}>
+                      {message.body}
+                    </div>
+                    <div className={`text-muted small ${message.ownedByCurrentUser ? 'text-right' : ''}`}>
+                      {message.ownedByCurrentUser ? 'You' : 'tempName'}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+            <div className="input-group">
+              <input
+                className="rounded form-control"
+                value={newMessage}
+                onChange={handleNewMessageChange}
+                placeholder="Write message..."
+              />
+              <div className="input-group-append">
+                <button onClick={handleSendMessage} className="btn btn-primary">Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    */
   )
 }
 
